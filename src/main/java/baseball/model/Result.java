e775daa7af48d318a24a9ee10bd13e609f510bee
wmw04087 <wmw04087@gmail.com>
2025-06-19 18:26:15 +0900

    feat: 결과(Result) 구현

---
+++ src/main/java/baseball/model/Result.java
@@ -0,0 +1,41 @@
+package baseball.model;
+
+import java.util.Objects;
+
+public class Result {
+
+    private static final int WINNING_STRIKE_COUNT = 3;
+
+    private final int strike;
+    private final int ball;
+
+    private Result(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public static Result of(int strike, int ball) {
+        return new Result(strike, ball);
+    }
+
+    public boolean isWin() {
+        return strike == WINNING_STRIKE_COUNT;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Result result = (Result) o;
+        return strike == result.strike && ball == result.ball;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(strike, ball);
+    }
+}
